852e38d4bfa68e0e675f7645f620a5ba6a6f1873
kimnanhee <kimnanhee0225@gmail.com>
2021-06-14 11:15:14 +0900

    [Modify] servo main, uart header

--- servo/servo/main.c
+++ servo/servo/main.c
@@ -1,62 +1,20 @@
-#define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <string.h>
+#include "uart.h"
 
-volatile unsigned char flag; // 블루투스 변수
-char data[10];
-int data_state = 0, motor_state = 0;
-
-char rx_char(void)
-{
-	while((UCSR0A & 0x80) == 0);
-	return UDR0;
-}
-
-void tx_char(char tx_char)
-{
-	while((UCSR0A & 0x20)==0);
-	UDR0 = tx_char; // 송신받을 값
-}
+int motor_state = 0;
 
 int main(void)
 {	
 	int i;
 	DDRD = 0xFF; // motor data port
 	
-	char txt;
-	UCSR0A = 0x00;
-	UCSR0B = 0x98;
-	UCSR0C = 0x06;
-	UBRR0H = 0x00;
-	UBRR0L = 103;
-	DDRB=0xF0;
-	DDRE = 0x08;
+	uart0_init(BAUDRATE(9600));	// Bluetooth connection
 	
     while (1) 
     {
-		txt = rx_char();
-		if(txt == 0x02) data_state = 1;
-		else if(txt == 0x03)
-		{
-			if(data[0] == 0x10) // rotate motor
-			{
-				
-			}
-			else if(data[1] == 0x11	) // stop motor
-			{
-				
-			}
-			memset(data, 0, sizeof(data));
-			data_state = 0;
-		}
-		else
-		{
-			if(data_state) strcat(data, txt);
-		}
-		tx_char(txt);
-		
 		if(motor_state)
 		{
 			for(i=0; i<4; i++)
@@ -66,4 +24,12 @@ int main(void)
 			}
 		}
     }
+}
+
+ISR(USART0_RX_vect)
+{
+	unsigned char buf = UDR0;
+	
+	if(buf == '0') motor_state = 0;
+	else if(buf == '1') motor_state = 1;
 }
---
+++ servo/servo/uart.h
@@ -0,0 +1,52 @@
+﻿#ifndef UART_H_
+#define UART_H_
+
+#define F_CPU 16000000UL
+#define BAUDRATE(x) ((F_CPU / 16 / x) - 1)
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+void uart0_init(unsigned int baud)
+{
+	UBRR0H = (unsigned char)(baud >> 8);
+	UBRR0L = (unsigned char)baud;
+	UCSR0B = (1 << TXEN0) | (1 << RXEN0) | (1<<RXCIE0);
+	sei();
+} // 8bit, no parity, 1 stop bit, TX enable, RX ISR enable
+
+void uart0_write(unsigned char data)
+{
+	while (!(UCSR0A & (1 << UDRE0)));
+	// wait for sending
+	UDR0 = data; // send
+}
+
+void uart0_string(char* str)
+{
+	while (*str)
+	uart0_write(*str++);
+}
+
+void uart1_init(unsigned int baud)
+{
+	UBRR1H = (unsigned char)(baud >> 8);
+	UBRR1L = (unsigned char)baud;
+	UCSR1B = (1 << TXEN1) | (1 << RXEN1) | (1<<RXCIE1);
+} // 8bit, no parity, 1 stop bit, TX enable, RX ISR enable
+
+void uart1_write(unsigned char data)
+{
+	while (!(UCSR1A & (1 << UDRE1)));
+	// wait for sending
+	UDR1 = data; // send
+}
+
+void uart1_string(char* str)
+{
+	while (*str)
+	uart1_write(*str++);
+}
+
+#endif /* UART_H_ */
